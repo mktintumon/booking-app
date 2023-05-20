b6675753eb0ab85772c6787bc18fcb63582500a5
mktintumon <mktintumon@gmail.com>
2023-05-20 23:34:44 +0530

    till login verify

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,9 @@
 import express from "express"
+import { register , login } from "../controllers/authController.js"
 const router = express.Router()
 
-router.get("/" , (req,res)=>{
-    res.send("this is auth section.")
-})
 
-router.get("/register" , (req,res)=>{
-    res.send("please register first")
-})
+router.post("/register",register)
+router.post("/login",login)
 
 export default router
